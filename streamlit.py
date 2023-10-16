b14c250fa929d8430216c3c2d6245cb11b9cb962
MehmetAltinkurt <mehmetaltinkurt@hotmail.com>
2023-10-16 23:38:24 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -39,7 +39,8 @@ if st.button("Get segmentation map"):
     if input_image:
         segments = process(input_image, backend)
         original_image = Image.open(input_image).convert("RGB")
-        segmented_image = Image.open(io.BytesIO(segments.content)).convert("RGB")
+        segmented_image = Image.open(io.BytesIO(segments.content))
+        segmented_image=segmented_image.convert("RGB")
         col1.header("Original")
         col1.image(original_image, use_column_width=True)
         col2.header("Segmented")
