35854d9bcde33a74d1754713fb1c470ef97a39ab
MehmetAltinkurt <mehmetaltinkurt@hotmail.com>
2023-10-19 12:33:24 +0300

    Update server.py

--- fastapi/server.py
+++ fastapi/server.py
@@ -11,12 +11,11 @@ app = FastAPI(
     description="""Obtain semantic segmentation maps of the image in input via YOLO5 implemented in PyTorch.""",
 )
 
-
+#Handle post requests
 @app.post("/segmentation")
 def get_segmentation_map(file: bytes = File(...)):
     """Get segmentation maps from image file"""
     segmented_image = get_segments(model, file)
     bytes_io = io.BytesIO()
     segmented_image.save(bytes_io, format="PNG")
-    print("post")
     return Response(bytes_io.getvalue(), media_type="image/png")
